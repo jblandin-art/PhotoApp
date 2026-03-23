5940e9f0c530011a22033632b26c259edc50df8a
lwhitwo1-star <lwhitwo1@charlotte.edu>
2026-03-23 19:52:40 -0400

    Update userDetail.jsx
    Added UserDetail view with link to user photos to satisfy user story for photo navigation

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -3,7 +3,9 @@ import {
   Typography
 } from '@mui/material';
 import './userDetail.css';
-
+import { Link } from 'react-router-dom';
+import {Box,Button,TextField} from '@mui/material';
+import fetchModel from "../../lib/fetchModelData";
 
 /**
  * Define UserDetail, a React component of project #5
@@ -11,18 +13,64 @@ import './userDetail.css';
 class UserDetail extends React.Component {
   constructor(props) {
     super(props);
+    this.state= {user: undefined};
   }
+componentDidMount(){
+const userId = this.props.match.params.userId;
 
+  fetchModel(`/user/${userId}`)
+  .then((response) => {
+    this.setState({user: response.data});
+  })
+  .catch((err) => {
+    console.error("Error fetching user:", err);
+  });
+}
+
+  
   render() {
-    return (
-      <Typography variant="body1">
-        This should be the UserDetail view of the PhotoShare app. Since
-        it is invoked from React Router the params from the route will be
-        in property match. So this should show details of user:
-        {this.props.match.params.userId}. You can fetch the model for the
-        user from window.models.userModel(userId).
-      </Typography>
-    );
+return this.state.user ? (
+  <div>
+  <Box component ="form" noValidate autoComplete ="off">
+    <div>
+      <Button variant= "contained" component={Link} to={`/photos/${this.state.user._id}`}>User Photos</Button>
+    </div>
+
+    <div>
+    <TextField id ="first_name" label = "First name" variant= "outlined" disabled fullWidth
+      margin ="normal" 
+      value={this.state.user.first_name}/>
+    </div>
+
+    <div>
+     <TextField id ="last_name" label = "Last name" variant= "outlined" disabled fullWidth
+      margin ="normal" 
+      value={this.state.user.last_name}/>
+    </div>
+
+    <div>
+        <TextField id ="location" label = "Location" variant= "outlined" disabled fullWidth
+      margin ="normal" 
+      value={this.state.user.location}/>
+    </div>
+
+    <div>
+        <TextField id ="description" label = "Description" variant= "outlined" disabled fullWidth
+      margin ="normal" 
+      value={this.state.user.description}/>
+    </div>
+
+    <div>
+        <TextField id ="occupation" label = "Occupation" variant= "outlined" disabled fullWidth
+      margin ="normal" 
+      value={this.state.user.occupation}/>
+    </div>
+  </Box>
+  </div>
+  
+) : (
+            <div/>
+        );
   }
 }
 
